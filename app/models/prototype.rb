4c867ebc0395508571ab287332dac3ad2c1e9d15
Daluma0344 <japan_masa11@icloud.com>
2023-03-13 15:22:32 +0900

    Prototypeモデルおよびテーブルを作成

---
+++ app/models/prototype.rb
@@ -0,0 +1,8 @@
+class Prototype < ApplicationRecord
+
+  #validates :title,         presence: true
+  #validates :catch_copy,      presence: true
+ # validates :con,   presence: true
+  #validates :position,     presence: true
+
+end
