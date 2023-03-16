ed40c69071eb16cb5c4922d43e6fb36f09e560b3
Daluma0344 <japan_masa11@icloud.com>
2023-03-16 18:05:36 +0900

    編集機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,21 @@ class PrototypesController < ApplicationController
   end
 
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+
+    if @prototype.update(prototype_params)
+      #binding.pry
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
